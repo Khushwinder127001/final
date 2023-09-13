c917a0aabb5f85b09a5513ad6cf2240820805bb5
Khushwinder127001) <khushwinder.php@gmail.com>
2023-09-13 23:49:49 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+axios.get("https://arstechnica.com/gadgets/").then(urlResponse =>{
+    const $ = cheerio.load(urlResponse.data);
+   
+ $('li.article').each((i, element)=>{
+    const link = $(element).find("a.overlay").attr("href");
+
+console.log(link);
+
+});
+});
+
+
+
+
+
