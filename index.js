dbfe60be15c89468fa8df6c8049be5711ba108af
Khushwinder127001) <khushwinder.php@gmail.com>
2023-09-14 00:10:17 +0530

    4

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ axios.get("https://arstechnica.com/gadgets/").then(urlResponse =>{
    
  $('li.article').each((i, element)=>{
     const link = $(element).find("a.overlay").attr("href");
-document.write(link);
 console.log(link);
+document.body.appendChild(link);
 
 });
 });
